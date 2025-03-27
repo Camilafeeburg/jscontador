9892f61ac7b4020f0acb9e5265929f914ee9673a
Camila <85712574004@senacrs.edu.br>
2025-03-27 12:15:02 -0300

    alteração

--- script.js
+++ script.js
@@ -5,16 +5,46 @@ var menosButton = document.getElementById('menos')
 let count = 0;
 let intervalId = 0;
 
-const updateValue = () => {
+const Novonum = () => {
     value.innerHTML = count;
 };
 
 maisButton.addEventListener('mousedown', () => {
     intervalId = setInterval(() => {
         count += 1;
-        updateValue()
-    }, 100)
+        Novonum();
+    }, 150);
 });
 
-document.addEventListener()
+maisButton.addEventListener('click' , () => {
+    count += 1;
+    Novonum();
+});
+
+
+
+menosButton.addEventListener('mousedown', () => {
+    intervalId = setInterval(() => {
+        count -= 1;
+    if (count <0){
+        count = 0;
+    }
+    Novonum();
+}, 150);
+});
+
+menosButton.addEventListener('click' , () => {
+   count -= 1;
+   if (count <0){
+    count = 0;
+   };
+   Novonum();
+});
+  
+
+
+document.addEventListener('mouseup' , () => {
+    clearInterval(intervalId);
+});
+
 
